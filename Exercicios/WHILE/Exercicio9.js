5cc1b49e2c6dfadfe049d51d6a270c6ea111c567
EZEQUIEL TEIXEIRA SANTOS <57824564851@info.local>
2024-05-14 10:01:20 -0300

    feat: Exercicio 9

---
+++ Exercicios/WHILE/Exercicio9.js
@@ -0,0 +1,10 @@
+// Faça um programa que receba um número do usuário e mostre no console a contagem regressiva até 0.
+
+let x = prompt("Insira um número: ");
+
+while (x >= 0)
+    {
+        console.log(x);
+        x--;
+    }
+
