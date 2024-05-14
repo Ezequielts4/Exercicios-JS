2ec0c46c9d6482d19efec3f825a4f1843834d159
EZEQUIEL TEIXEIRA SANTOS <57824564851@info.local>
2024-05-14 09:48:15 -0300

    feat: Exercicio 7

---
+++ Exercicios/WHILE/Exercicio7.js
@@ -0,0 +1,13 @@
+// Calcular a soma dos números de 1 a 100 utilizando a estrutura while.
+
+let x = 1;
+let soma = 0;
+
+while (x <= 100) 
+    {
+        soma += x;
+        x++;
+        console.log(soma);
+    }
+
+
