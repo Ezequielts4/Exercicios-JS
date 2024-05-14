d6c1734566911cdc97fdb313f424067d5acc3aba
EZEQUIEL TEIXEIRA SANTOS <57824564851@info.local>
2024-05-14 08:36:01 -0300

    feat: Exercicio 4

---
+++ Exercicios/Exercicio4.js
@@ -0,0 +1,11 @@
+// Faça um programa que imprima na tela a tabuada de um número
+// qualquer escolhido pelo usuário até o 10.
+
+// let n = prompt("Insira um número de 1 a 10: ")
+
+let numero = prompt("Insira um número de 1 a 10: ");
+
+for(let i = 0; i <= 10; i++)
+    {
+        console.log(numero * i);
+    }
