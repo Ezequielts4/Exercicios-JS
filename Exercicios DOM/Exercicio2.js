550e36cc8bf04a0513f82dd3558fc0ba83fc85df
EZEQUIEL TEIXEIRA SANTOS <57824564851@info.local>
2024-05-21 10:11:09 -0300

    feat: Exercicios DOM

---
+++ Exercicios DOM/Exercicio2.js
@@ -0,0 +1,24 @@
+// 2 - Adicionar e remover itens de uma lista
+
+const adicionar = () => {
+    let itemNovo = document.createElement("li")
+
+    itemNovo.textContent = "Item Adicionado"
+
+    let lista = document.getElementById("List")
+
+    lista.appendChild(itemNovo)
+}
+
+const remover = () => {
+    const list = document.getElementById("List");
+
+    if (list.children.length > 0) {   
+        list.removeChild(list.lastElementChild);
+    }
+
+    else{
+        alert("Não há mais itens...")
+    }
+}
+
